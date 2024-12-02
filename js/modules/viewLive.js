fe46198c57e84d8dc6f6f6d8fb99ed8f988f18a4
Glenn Cruz <cruzglennmark2000@gmail.com>
2024-12-02 13:35:11 +0800

    update viewlive.js

--- js/modules/viewLive.js
+++ js/modules/viewLive.js
@@ -1,36 +1,22 @@
-export function initViewLive() {
-    const viewLiveButtons = document.querySelectorAll(".view-live");
-
-    viewLiveButtons.forEach((button) => {
-        button.addEventListener("click", (event) => {
-            event.preventDefault(); // Prevent default anchor redirection
-            const url = button.getAttribute("data-url");
-
-            Swal.fire({
-                title: "Loading...",
-                html: "Please wait while we prepare the live view.",
-                allowOutsideClick: false,
-                didOpen: () => {
-                    Swal.showLoading();
-                },
-                timer: 2000,
-                timerProgressBar: true,
-            }).then(() => {
-                Swal.fire({
-                    title: "Are you sure?",
-                    text: "You will be redirected to the live view page.",
-                    icon: "question",
-                    showCancelButton: true,
-                    confirmButtonText: "Yes, proceed!",
-                    cancelButtonText: "No, stay here",
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = url;
-                    } else {
-                        console.log("User canceled the redirection.");
-                    }
-                });
-            });
-        });
-    });
-}
+let timerInterval;
+Swal.fire({
+  title: "Auto close alert!",
+  html: "I will close in <b></b> milliseconds.",
+  timer: 2000, // 2 seconds timer
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`; // Show remaining time in the <b> element
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval); // Clear the interval when the alert closes
+  }
+}).then((result) => {
+  // Handle dismissals after the alert closes
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log("I was closed by the timer");
+  }
+});
