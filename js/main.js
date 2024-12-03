6791fba12b8f69f456fa91d7bed657fbb5d021b9
Glenn Cruz <cruzglennmark2000@gmail.com>
2024-12-03 17:11:00 +0800

    updated main.js

--- js/main.js
+++ js/main.js
@@ -4,34 +4,36 @@ import { initNavigation } from './modules/navigation.js';
 import { initContactForm } from './modules/contact.js';
 import { initSkillBars } from './modules/skills.js';
 import { initModal } from './modules/modal.js';
+import { initTheme } from './modules/theme.js';
 
 // Initialize AOS
 AOS.init({
-    duration: 1000,
-    once: true,
-    offset: 100
+  duration: 1000,
+  once: true,
+  offset: 100,
 });
 
 // Initialize Typed.js
 new Typed('#typewriter', {
-    strings: [
-        'Front-End Developer',
-        'HTML Developer',
-        'CSS Developer',
-        'JavaScript Developer',
-        'UI/UX Enthusiast',
-        'React.js Developer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 30,
-    backDelay: 2000,
-    loop: true
+  strings: [
+    'Front-End Developer',
+    'HTML Developer',
+    'CSS Developer',
+    'JavaScript Developer',
+    'UI/UX Enthusiast',
+    'React.js Developer',
+  ],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true,
 });
 
 // Initialize modules
 document.addEventListener('DOMContentLoaded', () => {
-    initNavigation();
-    initContactForm();
-    initSkillBars();
-    initModal(); 
+  initNavigation();
+  initContactForm();
+  initSkillBars();
+  initModal();
+  initTheme();
 });
